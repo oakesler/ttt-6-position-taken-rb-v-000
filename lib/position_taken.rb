1ffeed610c7aecac375b0ed79bb5a0d8b82c4697
Olivia Kesler <oakesler@protonmail.ch>
2019-05-15 21:13:41 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+  x = board[index]
+  if x == " " || x == "" || x == nil
+    false
+  else x == "X" || x = "O"
+    true
+  end
+end
+
+
+
