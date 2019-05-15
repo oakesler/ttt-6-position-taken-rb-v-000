d7ee171dcbd11c92944c5089fd041b4007e453cb
Olivia Kesler <oakesler@protonmail.ch>
2019-05-15 21:12:38 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,12 +2,10 @@
 
 def position_taken?(board, index)
   x = board[index]
+  if x == " " || x == "" || x == nil
+    false
   if x == "X" || x = "O"
     true
-  elsif x == " " || x == "" || x == nil
-    false
-  else 
-    nil
   end
 end
 
